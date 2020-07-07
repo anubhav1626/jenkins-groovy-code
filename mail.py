98dc0309aa803e1cee4ec21748c77d3c37c2f42f
anubhav1626 <51701889+anubhav1626@users.noreply.github.com>
2020-07-07 21:40:46 +0530

    Update mail.py

--- mail.py
+++ mail.py
@@ -4,7 +4,7 @@ s = smtplib.SMTP('smtp.gmail.com', 587)
 # start TLS for security 
 s.starttls()   
 # Authentication 
-s.login("anubhavpahwa2608@gmail.com", "anubhav@26") 
+s.login("anubhavpahwa2608@gmail.com", "rjtpniliokrfmylc") 
 # message to be sent 
 message = "Hey Developer,Our website is up."
 # sending the mail 
